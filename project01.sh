1d5b3291f2b9871673afa94e2736ba702b25a80e
Innocenzio Rizzuto <Ir8643@g.rit.edu>
2023-10-22 12:49:22 -0400

    start and kill processes work

--- project01.sh
+++ project01.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+files=$(ls | cut -d "," -f 1,2,3,4,5,6)
+
+spawn() {
+    for i in $files; do
+        ./$i 129.21.141.90 &
+    done
+}
+
+finish() {
+        for pid in $(ps | egrep "APM" | awk '{print $1}'); do
+            kill "$pid"
+            echo "Killed $pid"
+        done
+}

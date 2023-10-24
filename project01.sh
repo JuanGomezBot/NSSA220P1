1778c8cdc1b585b43779ef114e4453998e2f730c
Innocenzio Rizzuto <Ir8643@g.rit.edu>
2023-10-24 02:00:29 -0400

    added collect function

--- project01.sh
+++ project01.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-files=$(ls | cut -d "," -f 1,2,3,4,5,6)
+files=$(ls | egrep "APM" | cut -d "," -f 1,2,3,4,5,6)
 
 spawn() {
     for i in $files; do
@@ -9,8 +9,30 @@ spawn() {
 }
 
 finish() {
-        for pid in $(ps | egrep "APM" | awk '{print $1}'); do
-            kill "$pid"
-            echo "Killed $pid"
-        done
+    for pid in $(ps | egrep "APM" | awk '{print $1}'); do
+        kill $pid
+    done
 }
+
+collect() {
+    #keep processes running and collect data and increment sampling using sleep
+    count=0
+    while (true); do
+        if [[ $count = 180 ]]
+        then
+            echo "Collection done"
+            break
+        fi
+
+        iostat
+        sleep 5
+        ((count++))
+        secs=$(( $count * 5 ))
+        echo "$secs seconds have passed"
+        echo "$count sample(s) have been taken"
+    done
+}
+
+spawn
+collect
+finish

6d3df6b1698cd036bcf4616a8ef9eb4737677e6b
JuanGomezBot <89549333+JuanGomezBot@users.noreply.github.com>
2023-10-27 00:01:53 -0400

    Modified the sys_metrics, tomorrow I will see professor arcoraci

--- project01.sh
+++ project01.sh
@@ -13,7 +13,25 @@ finish() {
         kill $pid
     done
 }
+sys_metrics(){
 
+
+#Collects memory utilities
+#It install the sysstat which is necessary for the iostat and to remove all the output it will use -q and -y to authorize it
+sudo yum install -q -y sysstat
+
+# read network bandwith {I use the ifstat on the ens33 and make it read only the 4th field and move it to netbandwith.txt it will find the
+ifstat -t 1 | grep ens33 | awk '{print  $7 ","  $9}' >> ens33_netbandwidth.csv
+
+#Uses iostat to read the hard disk access rate in kb/s and redirect it to sda_metrics
+iostat -k  | grep sda | awk '{print $4}' >> sda_hard_disk_access_metrics.csv
+# 
+ps  | grep 'APM' | awk '{print $2 "," $3 "," $4}' >> "process_metrics/process_metrics.csv"
+
+df / -m | tail -n 1 | awk '{print  $3 }' >> hard_disk_available_metrics.csv
+
+sleep 5
+}
 collect() {
     #keep processes running and collect data and increment sampling using sleep
     count=0
@@ -23,13 +41,17 @@ collect() {
             echo "Collection done"
             break
         fi
-
-        iostat
+        
+        sys_metrics
         sleep 5
         ((count++))
         secs=$(( $count * 5 ))
         echo "$secs seconds have passed"
         echo "$count sample(s) have been taken"
+        
+        if [ $secs -eq 900 ]; then
+        break
+        fi
     done
 }
 
